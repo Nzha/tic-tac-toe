71384e8dc772327c2a4053842c09da528209a9d0
Adrien <adrien.biz@gmail.com>
2022-09-20 21:31:11 +0400

    Add _ for private properties

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,16 @@
 const gameboard = (() => {
-
-    const gameboardDiv = document.querySelector('.gameboard');
+    const _gameboardDiv = document.querySelector('.gameboard');
 
     // Create a 3*3 array with 0 as values
-    const array = new Array(3).fill(new Array(3).fill(0));
+    const _array = new Array(3).fill(new Array(3).fill(0));
 
     // Add a div to each space to display gameboard
     const display = () => {
-        for (const row of array) {
+        for (const row of _array) {
             for (space of row) {
                 const newSpace = document.createElement('div');
                 newSpace.classList.add('space');
-                gameboardDiv.appendChild(newSpace);
+                _gameboardDiv.appendChild(newSpace);
             }
         }
     };
@@ -19,7 +18,6 @@ const gameboard = (() => {
     return {
         display
     };
-
 })();
 
 gameboard.display();
